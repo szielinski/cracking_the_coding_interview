b8102f08504a7ac099828d14d6c6c65ccc4f854f
Szymon Zielinski <szymon.zielinski14@gmail.com>
2017-10-17 19:02:14 +0100

    Add Successor challenge

---
+++ graphs_and_trees/interview_questions/Successor.java
@@ -0,0 +1,63 @@
+/*
+ * Validate BST: Implement a function to check if a binary tree is a binary search tree.
+ */
+
+import java.util.*;
+import java.math.*;
+
+public class Successor {
+
+    // left subtree <= root < right subtree
+    static Node nextNode(Node n){
+        if(n == null) return null;
+
+        if(n.rightChild != null){
+            return leftmostChild(n.rightChild);
+        }
+
+        Node current = n;
+        Node parent = n.parent;
+        while(parent != null && current == parent.rightChild){
+            current = parent;
+            parent = current.parent;
+        }
+        return parent;
+    }
+
+    static Node leftmostChild(Node n){
+        while(n.leftChild != null){
+            n = n.leftChild;
+        }
+        return n;
+    }
+
+    public static void main(String[] args){
+        Node binaryTree = new Node(12, null);
+        binaryTree.leftChild = new Node(8, binaryTree);
+        binaryTree.rightChild = new Node(14, binaryTree);
+        binaryTree.rightChild.leftChild = new Node(13, binaryTree.rightChild);
+        binaryTree.leftChild.leftChild = new Node(5, binaryTree.leftChild);
+        binaryTree.leftChild.rightChild = new Node(10, binaryTree.leftChild);
+        System.out.println(nextNode(binaryTree));
+        System.out.println(nextNode(binaryTree.leftChild));
+        System.out.println(nextNode(binaryTree.rightChild));
+        System.out.println(nextNode(binaryTree.rightChild.leftChild));
+        System.out.println(nextNode(binaryTree.leftChild.rightChild));
+    }
+}
+
+class Node {
+    Node leftChild;
+    Node rightChild;
+    Node parent;
+    int data;
+
+    Node(int data, Node parent){
+        this.data = data;
+        this.parent = parent;
+    }
+
+    public String toString(){
+        return Integer.toString(data);
+    }
+}
